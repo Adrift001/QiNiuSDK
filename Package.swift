8f0019b874c5223e170ca194f0812de15d260d63
Optional <me@jingxuetao.com>
2020-05-17 13:43:30 +0800

    update package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,8 +14,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.1"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.1.1"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
     ],
     targets: [
@@ -23,7 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "QiNiuSDK",
-            dependencies: ["CryptoSwift", "AsyncHTTPClient", "Logging"]),
+            dependencies: [
+                .product(name: "AsyncHTTPClient", package: "async-http-client")
+                .product(name: "Crypto", package: "swift-crypto"),
+                .product(name: "Logging", package: "swift-log")
+        ]),
         .testTarget(
             name: "QiNiuSDKTests",
             dependencies: ["QiNiuSDK"]),
