1828824b5f5a0c28c64c9ed754ce8f9c7e0d2ab5
Optional <me@jingxuetao.com>
2020-06-19 16:18:13 +0800

    Revert "上传服务器地址."
    This reverts commit 84ef1af0b140fcd281603d00390220f79773c2bb.

--- Sources/QiNiuSDK/Define.swift
+++ Sources/QiNiuSDK/Define.swift
@@ -28,29 +28,3 @@ public enum FileStoreType: Int {
     case standard = 0
     case low = 1
 }
-
-/// 上传服务器域名
-public enum UploadServer: String {
-    /// 华东一
-    case ec1 = "up.qiniup.com"
-    /// 华东二
-    case ec2 = "up-z0.qiniup.com"
-    /// 华东三
-    case ec3 = "upload.qiniup.com"
-    /// 华北一
-    case nc1 = "up-z1.qiniup.com"
-    /// 华北二
-    case nc2 = "upload-z1.qiniup.com"
-    /// 华南一
-    case sc1 = "up-z2.qiniup.com"
-    /// 华南二
-    case sc2 = "upload-z2.qiniup.com"
-    /// 北美一
-    case na1 = "up-na0.qiniup.com"
-    /// 北美二
-    case na2 = "upload-na0.qiniup.com"
-    /// 东南亚一
-    case sa1 = "up-as0.qiniup.com"
-     /// 东南亚二
-    case sa2 = "upload-as0.qiniup.com"
-}
